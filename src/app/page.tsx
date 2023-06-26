d0994ff746e4bb23a94160f3ed03c3bb34ad7b51
Oliver Looney <oliverjlooney@gmail.com>
2023-06-27 00:05:42 +0100

    basic mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import {leaderboard} from "@/utils/LeaderboardWins";
 import LeaderboardTable from "@/component/LeaderboardTable";
+import Timeline from "@/component/Timeline";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <br/>
       <LeaderboardTable/>
       <br/>
+        <Timeline/>
     </main>
   )
 }
---
+++ src/component/Timeline.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Chrono } from 'react-chrono';
+import { leaderboard } from '@/utils/LeaderboardWins';
+
+const Timeline = () => {
+    const timelineItems = leaderboard.wins.map((win) => ({
+        title: win.name,
+        cardTitle: win.name,
+        cardSubtitle: win.date.toDateString(),
+        media: {
+            type: 'IMAGE',
+            source: {
+                url: `${win.name.toLowerCase()}.png`,
+            },
+        },
+    }));
+
+    return (
+        <div className="timeline-container">
+            <Chrono
+                items={timelineItems}
+                mode="VERTICAL_ALTERNATING"
+                hideControls // Remove the "Read more" controls
+                allowDynamicUpdate // Allow dynamic updates when the timeline items change
+                theme={{
+                    primary: '#ffffff', // Set the background color to white
+                    secondary: '#000000', // Set the text color to black
+                    cardBgColor: '#ffffff', // Set the card background color to white
+                    cardForeColor: '#000000', // Set the card text color to black
+                }}
+            />
+        </div>
+    );
+};
+
+export default Timeline;
