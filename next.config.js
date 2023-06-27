4c151996c72b44684ae1f10f06abab8d66600f3e
Oliver Looney <oliverjlooney@gmail.com>
2023-06-27 09:20:36 +0100

    updatin next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
-}
+    output: {
+        standalone: true,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
